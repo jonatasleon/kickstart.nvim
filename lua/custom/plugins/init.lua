2c1fa114be253e7345ea1d0fb649ea5ec415de9f
Jonatas Leon <jonatas.leon@gmail.com>
2023-03-07 20:45:55 -0300

    Update setup config for nvim-tree plugin.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -21,7 +21,13 @@ return {
     },
     config = function()
       require('nvim-tree').setup {
-        sort_by = "case_sensitive"
+        sort_by = "case_sensitive",
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
       }
     end
   },
