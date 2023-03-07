277f69415ca6c9619ad39fdf8e76a49d627f57bf
Jonatas Leon <jonatas.leon@gmail.com>
2023-03-07 17:31:49 -0300

    Add several plugins: "beacon.nvim", "copilot.vim", "zen-mode.nvim", "nvim-bufbar", "nvim-tree.lua", "better-escape.nvim", "nvim-autopairs", and "tmux.nvim". Also add configurations for each plugins.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,55 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'danilamihailov/beacon.nvim',
+  'github/copilot.vim',
+  'folke/zen-mode.nvim',
+
+  {
+    'ojroques/nvim-bufbar',
+    config = function()
+      require('bufbar').setup()
+    end
+  },
+
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {
+        sort_by = "case_sensitive"
+      }
+    end
+  },
+
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup {
+        mapping = { "jk", "kj" },   -- a table with mappings to use
+        timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+        clear_empty_lines = false,  -- clear line after escaping if there is only whitespace
+        -- keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+        -- example(recommended)
+        keys = function()
+          return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+        end,
+      }
+    end,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup {}
+    end
+  },
+
+  {
+    "aserowy/tmux.nvim",
+    config = function() return require("tmux").setup() end
+  }
+}
