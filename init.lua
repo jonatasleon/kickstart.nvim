2f2c998b94a0bd03441181ea374f4386abe3c863
Jonatas Leon <jonatas.leon@gmail.com>
2023-03-12 02:03:24 -0300

    Remove unnecessary language installations from nvim-treesitter setups.

--- init.lua
+++ init.lua
@@ -153,7 +153,7 @@ vim.keymap.set('n', '<leader>e', require('nvim-tree').focus, { desc = 'Focus fil
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim', 'toml' },
+  ensure_installed = { 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim', 'toml' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
