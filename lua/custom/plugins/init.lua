85109a02751495f78041fbef963d730b3160fcee
Jonatas Leon <jonatas.leon@gmail.com>
2023-03-12 01:56:17 -0300

    Added new dependencies for the plugin functionality.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -59,7 +59,14 @@ return {
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
   },
 
   -- Useful plugin to show you pending keybinds.
