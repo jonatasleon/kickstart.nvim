89e04c2b09f742e41506235e12fe10a176b649d7
Jonatas Leon <jonatas.leon@gmail.com>
2023-03-12 02:02:14 -0300

    Fix telescope keymap indentation.

--- init.lua
+++ init.lua
@@ -118,8 +118,8 @@ require('telescope').setup {
   defaults = {
     mappings = {
       i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
       },
     },
   },
